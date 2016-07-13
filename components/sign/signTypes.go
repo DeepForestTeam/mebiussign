e0aa4dab96112fda0da632d98e89e432aea66e9e
mcmx <mcmx@mcmx>
2016-07-13 22:54:46 +0300

    Sign WIP

--- components/sign/signTypes.go
+++ components/sign/signTypes.go
@@ -3,30 +3,33 @@ package sign
 import "time"
 
 type SignatureRow struct {
+	//
 	RowIndex        string    `json:"key"`
-	TimeStamp       time.Time `json:"time_stamp"`
-	TimeStampHash   string    `json:"time_hash"`
 	//USER PROVIDED DATA
 	DataUrl         string `json:"data_url"`
 	DataNote        string `json:"data_note"`
-	DataSignature   string `json:"data_signature"`
+	DataHash        string `json:"data_hash"`
 	DataBlock       []byte `json:"data_block"`
 	//System given info
+	TimeStamp       time.Time `json:"time_stamp"`
+	TimeStampHash   string    `json:"time_hash"`
 	PrevKey         string `json:"prev_key"`
 	PrevSignature   string `json:"prev_signature"`
 	MobiusSignature string `json:"mobius_sign"`
 }
 
 type SignatureRequest struct {
-	ObjectID      string `json:"object_id"`
-	TimeStampHash string `json:"time_hash"`
+	//Service and object identification
+	ServiceID     string        `json:"service_id"`
+	ObjectID      string        `json:"object_id"`
+	ConsumerID    string        `json:"object_id"`
 	//DATA SECTION
-	DataUrl       string `json:"data_url"`
-	DataNote      string `json:"data_note"`
-	DataSignature string `json:"data_signature"`
-	DataBlock     []byte `json:"data_block"`
+	DataUrl       string        `json:"data_url"`
+	DataNote      string        `json:"data_note"`
+	DataSignature string        `json:"data_signature"`
+	DataBlock     []byte        `json:"data_block"`
 	//Security Section
-	VendorSign    string
+	ServiceSign   string        `json:"service_sign"`
 }
 
 type MobiusTape struct {
